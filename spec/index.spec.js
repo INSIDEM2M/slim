56cd0d71cd91b454166deee657902eef953ef8db
Frederik Schubert <frederik.schubert@inside-m2m.de>
2016-12-13 23:10:23 +0100

    feat(build): implement basic e2e test
    Closes https://github.com/INSIDEM2M/slim/issues/17

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,1 +1,30 @@
-return 0;
+var spawn = require("child_process").spawn;
+var path = require("path");
+
+var cwd = process.cwd();
+
+function installExampleProject() {
+    return new Promise(function (resolve) {
+        spawn("yarn", ["install"], {
+            cwd: path.join(cwd, "e2e", "example-project"),
+            stdio: "inherit"
+        }).on("close", function (code) {
+            resolve(code);
+        });
+    });
+}
+
+function buildExampleProject() {
+    return new Promise(function (resolve) {
+        spawn("yarn", ["run", "e2e"], {
+            cwd: path.join(cwd, "e2e", "example-project"),
+            stdio: "inherit"
+        }).on("close", function (code) {
+            resolve(code);
+        });
+    });
+}
+
+installExampleProject().then(buildExampleProject).then(function (code) {
+    process.exit(code);
+});
