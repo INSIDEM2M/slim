57fae28004939c49942459d72035efb928309ec6
Sven Pfennig <sven.pfennig@inside-m2m.de>
2017-01-17 21:26:50 +0100

    feat(newCommand): replace nodegit by simplegit

--- src/commands/new.command.ts
+++ src/commands/new.command.ts
@@ -4,7 +4,7 @@ import * as path from "path";
 import * as os from "os";
 
 let builder: yargs.CommandBuilder = ((): yargs.CommandBuilder => {
-    
+
     let templates: yargs.CommandBuilder = {
         "component": {
             alias: "c",
@@ -25,7 +25,7 @@ let builder: yargs.CommandBuilder = ((): yargs.CommandBuilder => {
     };
 
     try {
-        templates = require(path.join(os.homedir(),".slim/templates.ts")).templates;
+        templates = require(path.join(os.homedir(), ".slim/templates.ts")).templates;
         logger.info(".slim/templates.ts:\n" + JSON.stringify(templates, null, 2));
     } catch (error) {
         logger.info(error);
@@ -34,7 +34,7 @@ let builder: yargs.CommandBuilder = ((): yargs.CommandBuilder => {
 
     //TODO: (0xe282b0) check that templates variable is a valid CommandBuilder;
     //TODO: (0xe282b0) check that aliases are unique;
-   
+
     return templates;
 })();
 
@@ -59,81 +59,75 @@ export const newCommand: yargs.CommandModule = {
 
         ).then(function (answers) {
 
-            let nodegit = require("nodegit");
             let fs = require('fs');
 
             let targetPath = path.join(os.homedir(), ".slim", answers.project.type);
-            
-            if (fs.existsSync(targetPath)) {  
 
-                nodegit.Repository.open(targetPath)
-                    .then((repo) => repo.fetchAll())
-                    .then((repo) => repo.mergeBranches("master", "origin/master"))
-                    .then(()=>scaffold(targetPath, process.cwd()),()=>{console.log("error")})
+            var simpleGit = require('simple-git')(targetPath);
+            if (fs.existsSync(targetPath)) {
+
+                simpleGit.pull('origin', 'master', { '--rebase': 'true' })
+                    .then(() => scaffold(targetPath, process.cwd()), () => { console.log("error") })
 
                 console.log("check if local availabe");
                 console.log("pull if availabe");
-            }else{
+            } else {
                 console.log("clone if not available");
-                nodegit.Clone(
-                builder[answers.project.type].url,
-                targetPath,
-                {
-                    fetchOpts: {
-                        callbacks: {
-                            certificateCheck: function () {
-                                // github will fail cert check on some OSX machines
-                                // this overrides that check
-                                return 1;
-                            }
-                        }
-                    }
-                })
-                .then(()=>scaffold(targetPath, process.cwd()))
+                simpleGit.clone(
+                    builder[answers.project.type].url,
+                    targetPath)
+                    .then(() => scaffold(targetPath, process.cwd()))
             }
-        });                
+        });
 
     }
 };
 
-const scaffold = (source,target)=>{
-                let _ = require('underscore');
-                let fs = require('fs');
-                var glob = require("glob")
+const scaffold = (source, target) => {
+    let _ = require('underscore');
+    let fs = require('fs');
+    var glob = require("glob")
 
-                let templateConfig = require(path.join(source,"template.ts")).config;
+    let templateConfig = require(path.join(source, "template.ts")).config;
 
-                
 
-                var inquirer = require('inquirer');
-                inquirer.prompt(templateConfig.questions)
-                .then((answers)=>new Promise((resolve,jeject)=>resolve(templateConfig.mapper(answers))))
-                .then(answers=>new Promise((resolve,reject)=>{
 
-                    glob(templateConfig.include, {cwd:source}, function (er, files) {
+    var inquirer = require('inquirer');
+    inquirer.prompt(templateConfig.questions)
+        .then((answers) => new Promise((resolve, jeject) => resolve(templateConfig.mapper(answers))))
+        .then(answers => new Promise((resolve, reject) => {
 
-                        for(let f of files){
-                            console.log(f);
-                            fs.writeFile(path.join(target,f), 
-                            _.template(fs.readFileSync(path.join(source,f), { 'encoding':'utf8'}))(answers),
-                             function(err) {
-                                if(err) {
+            glob("**", { cwd: source, ignore: templateConfig.include }, function (er, files) {
+                for (let f of files) {
+                    console.log(f);
+                    fs.createReadStream(path.join(source, f)).pipe(fs.createWriteStream(path.join(target, f)));
+                }
+
+                glob(templateConfig.include, { cwd: source }, function (er, files) {
+
+                    for (let f of files) {
+                        console.log(f);
+                        fs.writeFile(path.join(target, f),
+                            _.template(fs.readFileSync(path.join(source, f), { 'encoding': 'utf8' }))(answers),
+                            function (err) {
+                                if (err) {
                                     return console.log(err);
                                 }
 
                                 console.log("The file was saved!");
 
-                            }); 
-                        }
+                            });
+                    }
 
-                                resolve(0);
-                    });
-                }))
+                    resolve(0);
+                });
+            });
+        }))
 
-                console.log("Read template.js");
-                console.log("Check if description is complete");
-                console.log("run before scaffolding script (check deps)");
-                console.log("ask questions");
-                console.log("replace placeholders");
-                console.log("run after scaffolding script");
+    console.log("Read template.js");
+    console.log("Check if description is complete");
+    console.log("run before scaffolding script (check deps)");
+    console.log("ask questions");
+    console.log("replace placeholders");
+    console.log("run after scaffolding script");
 }
