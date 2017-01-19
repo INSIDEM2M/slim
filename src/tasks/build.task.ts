88d8ef936ee9c74bc138198a574ad430363ed1ce
Frederik Schubert <frederik.schubert@inside-m2m.de>
2017-01-19 12:13:21 +0100

    fix(build): delete targetDir before build
    Closes https://github.com/INSIDEM2M/slim/issues/81

--- src/tasks/build.task.ts
+++ src/tasks/build.task.ts
@@ -1,6 +1,7 @@
 import * as path from "path";
 import * as webpack from "webpack";
 import * as webpackMerge from "webpack-merge";
+import * as rimraf from "rimraf";
 
 import { getCommonConfigPartial } from "../webpack/webpack.config.common";
 import { getBuildConfigPartial } from "../webpack/webpack.config.build";
@@ -8,6 +9,8 @@ import { timer, logger } from "../logger";
 import { SlimConfig } from "../config/slim-config/slim-config";
 
 module.exports = function (env: EnvironmentVariables, config: SlimConfig, minify: boolean, aot: boolean) {
+    rimraf.sync(config.targetDir);
+    logger.debug("Deleted " + config.targetDir);
     const indexPath = path.join(config.sourceDir, "index.html");
     const commonConfig = getCommonConfigPartial(indexPath, env, config);
     const buildConfig = getBuildConfigPartial(config, minify, aot, indexPath);
