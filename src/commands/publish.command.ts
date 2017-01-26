b45fc6d44beb40c349276d7c9931b282c38224a2
Frederik Schubert <frederik.schubert@inside-m2m.de>
2017-01-26 19:54:49 +0100

    feat(publish): add initial publish command implementation
    Closes https://github.com/INSIDEM2M/slim/issues/104

--- src/commands/publish.command.ts
+++ src/commands/publish.command.ts
@@ -1,10 +1,28 @@
 import * as yargs from "yargs";
 import { logger } from "../utils";
+import { spawn } from "child_process";
+import * as simpleGit from "simple-git";
 
 export const publishCommand: yargs.CommandModule = {
     command: "publish",
     describe: "Publish the package to the npm registry and the central git repository.",
     handler: (options: Options) => {
-        logger.error("Not yet implemented!");
+        const rootDir = process.cwd();
+        const npmPublishCommand = spawn("npm", ["publish"], {
+            stdio: "inherit"
+        });
+        npmPublishCommand.on("exit", exitCode => {
+            if (exitCode === 0) {
+                logger.info("Published package to the npm registry.");
+                simpleGit(rootDir)
+                    .push("origin")
+                    .pushTags("origin")
+                    .then(() => logger.info("Pushed the package to the git repository."))
+                    .then(() => process.exit(0));
+            } else {
+                logger.error("Could not publish package!");
+                process.exit(exitCode);
+            }
+        });
     }
 };
