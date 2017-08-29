6bc3ddb115743cebcb268b2f5b7219583769d5cc
Frederik Schubert <frederik.schubert@inside-m2m.de>
2017-08-29 15:55:46 +0200

    fix(build): update to newest @ngtools/webpack

--- src/cli.ts
+++ src/cli.ts
@@ -15,87 +15,90 @@ import * as os from "os";
 import { spawn } from "child_process";
 import { logger } from "./utils";
 import { argv } from "yargs";
+import { gt } from "semver";
 
 export function main() {
+  const pkg = require(path.join(__dirname, "..", "package.json"));
 
-    const pkg = require(path.join(__dirname, "..", "package.json"));
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
+  rl.on("SIGINT", () => {
+    process.exit(0);
+  });
 
-    rl.on("SIGINT", () => {
-        process.exit(0);
-    });
+  if (!argv["ci"]) {
+    const slimPath = path.join(os.homedir(), ".slim");
+    const metadataPath = path.join(slimPath, ".metadata.json");
+    let slimMetadata;
 
-    if (!argv["ci"]) {
-        const slimPath = path.join(os.homedir(), ".slim");
-        const metadataPath = path.join(slimPath, ".metadata.json");
-        let slimMetadata;
-
-        if (!fs.existsSync(slimPath)) {
-            fs.mkdirSync(slimPath);
-            slimMetadata = {};
-        } else {
-            if (fs.existsSync(metadataPath)) {
-                slimMetadata = require(metadataPath);
-            } else {
-                slimMetadata = {};
-            }
-        }
-
-        if (!slimMetadata.lastVersionUpdateCheck || new Date().getTime() - new Date(slimMetadata.lastVersionUpdateCheck).getTime() > 1000 * 60 * 60 * 24) {
-            checkForUpdate(pkg, metadataPath, slimMetadata);
-        }
+    if (!fs.existsSync(slimPath)) {
+      fs.mkdirSync(slimPath);
+      slimMetadata = {};
+    } else {
+      if (fs.existsSync(metadataPath)) {
+        slimMetadata = require(metadataPath);
+      } else {
+        slimMetadata = {};
+      }
     }
 
-    yargs
-        .locale("en")
-        .version(pkg.version)
-        .usage("Usage: $0 <command> [options]")
-        .command(newCommand)
-        .command(devCommand)
-        .command(buildCommand)
-        .command(testCommand)
-        .command(e2eCommand)
-        .command(docCommand)
-        .command(releaseCommand)
-        .command(publishCommand)
-        .alias("h", "help")
-        .alias("v", "version")
-        .option("environment", {
-            alias: "env",
-            description: "Use the buildProperties of a different environment from the package.json than defined via NODE_ENV."
-        })
-        .option("ci", {
-            description: "Run slim in a Continuous Integration environment mode. This produces file outputs for tests and reduces logging."
-        })
-        .demand(1)
-        .epilog(`Run ${chalk.bold("$0 <command> --help")} for more information on the specific command.`)
-        .help().argv;
+    if (
+      !slimMetadata.lastVersionUpdateCheck ||
+      new Date().getTime() - new Date(slimMetadata.lastVersionUpdateCheck).getTime() > 1000 * 60 * 60 * 24
+    ) {
+      checkForUpdate(pkg, metadataPath, slimMetadata);
+    }
+  }
+
+  yargs
+    .locale("en")
+    .version(pkg.version)
+    .usage("Usage: $0 <command> [options]")
+    .command(newCommand)
+    .command(devCommand)
+    .command(buildCommand)
+    .command(testCommand)
+    .command(e2eCommand)
+    .command(docCommand)
+    .command(releaseCommand)
+    .command(publishCommand)
+    .alias("h", "help")
+    .alias("v", "version")
+    .option("environment", {
+      alias: "env",
+      description: "Use the buildProperties of a different environment from the package.json than defined via NODE_ENV."
+    })
+    .option("ci", {
+      description: "Run slim in a Continuous Integration environment mode. This produces file outputs for tests and reduces logging."
+    })
+    .demand(1)
+    .epilog(`Run ${chalk.bold("$0 <command> --help")} for more information on the specific command.`)
+    .help().argv;
 }
 
 function checkForUpdate(pkg: any, metadataPath: string, slimMetadata: any) {
-    logger.debug("Checking for newer slim version...");
-    return getLatestSlimVersion(pkg).then(version => {
-        if (pkg.version !== version) {
-            console.log("\n");
-            logger.info("---------------- UPDATE AVAILABLE! ----------------");
-            logger.info(`There is a new version of slim available! Current version: ${pkg.version}. Latest version: ${version}.`);
-            logger.info(`Run ${chalk.bold("npm i -g slim-cli")} to update.`);
-            console.log("\n");
-        }
-        slimMetadata.lastVersionUpdateCheck = new Date();
-        fs.writeFileSync(metadataPath, JSON.stringify(slimMetadata, null, 4));
-    });
+  logger.debug("Checking for newer slim version...");
+  return getLatestSlimVersion(pkg).then(version => {
+    if (gt(version, pkg.version)) {
+      console.log("\n");
+      logger.info("---------------- UPDATE AVAILABLE! ----------------");
+      logger.info(`There is a new version of slim available! Current version: ${pkg.version}. Latest version: ${version}.`);
+      logger.info(`Run ${chalk.bold("npm i -g slim-cli")} to update.`);
+      console.log("\n");
+    }
+    slimMetadata.lastVersionUpdateCheck = new Date();
+    fs.writeFileSync(metadataPath, JSON.stringify(slimMetadata, null, 4));
+  });
 }
 
 function getLatestSlimVersion(pkg: any): Promise<string> {
-    return new Promise(resolve => {
-        const npmCommand = spawn("npm", ["show", pkg.name, "version"]);
-        npmCommand.stdout.on("data", (version) => {
-            resolve(version.toString().trim());
-        });
+  return new Promise(resolve => {
+    const npmCommand = spawn("npm", ["show", pkg.name, "version"]);
+    npmCommand.stdout.on("data", version => {
+      resolve(version.toString().trim());
     });
+  });
 }
--- src/tasks/build.task.ts
+++ src/tasks/build.task.ts
@@ -12,52 +12,79 @@ import { argv } from "yargs";
 import * as fs from "fs";
 import { extractBundles } from "../webpack/plugins/chunk.plugin";
 
-module.exports = function (env: Environment, config: SlimConfig, minify: boolean, aot: boolean, skipSourceMaps: boolean, codesplit: boolean) {
-    rimraf.sync(config.targetDir);
-    logger.debug("Deleted " + config.targetDir);
-    const indexPath = path.join(config.sourceDir, "index.html");
-    const commonConfig = getCommonConfigPartial(indexPath, env, config, false, aot);
-    const buildConfig = getBuildConfigPartial(config, minify, indexPath, skipSourceMaps);
-    const codesplitConfig = codesplit ? extractBundles([{
-        name: "vendor",
-        minChunks: isVendor // extract node_modules
-    }, {
-        name: "manifest" // extract webpack runtime code
-    }]) : undefined;
-    const webpackConfig = webpackMerge.smart(commonConfig, buildConfig, codesplitConfig);
-    logger.debug("Created webpack build config.", prettyPrintConfig(webpackConfig));
-    logger.info(`Building ${minify ? "minified " : ""}application${aot ? " using the Angular AOT compiler" : ""}...`);
-    return runBuild(webpackConfig, config);
+module.exports = function(
+  env: Environment,
+  config: SlimConfig,
+  minify: boolean,
+  aot: boolean,
+  skipSourceMaps: boolean,
+  codesplit: boolean
+) {
+  rimraf.sync(config.targetDir);
+  logger.debug("Deleted " + config.targetDir);
+  const indexPath = path.join(config.sourceDir, "index.html");
+  const commonConfig = getCommonConfigPartial(indexPath, env, config, false, aot);
+  const buildConfig = getBuildConfigPartial(config, minify, indexPath, skipSourceMaps);
+  const codesplitConfig = codesplit
+    ? extractBundles([
+        {
+          name: "vendor",
+          minChunks: isVendor // extract node_modules
+        },
+        {
+          name: "manifest" // extract webpack runtime code
+        }
+      ])
+    : undefined;
+  const webpackConfig = webpackMerge.smart(commonConfig, buildConfig, codesplitConfig);
+  logger.debug("Created webpack build config.", prettyPrintConfig(webpackConfig));
+  logger.info(`Building ${minify ? "minified " : ""}application${aot ? " using the Angular AOT compiler" : ""}...`);
+  return runBuild(webpackConfig, config);
 };
 
 function isVendor({ resource }) {
-    return resource &&
-        resource.indexOf("node_modules") >= 0 &&
-        resource.match(/\.js$/);
+  return resource && resource.indexOf("node_modules") >= 0 && resource.match(/\.js$/);
+}
+
+function onlyTemplateErrors(errors: string[]) {
+  return errors.reduce((prev, curr) => prev && isTemplateError(curr), true);
+}
+
+function isTemplateError(error: string) {
+  return error.startsWith("ng://") || error.includes("$$_gendir");
 }
 
 function runBuild(config: webpack.Configuration, conf: SlimConfig) {
-    timer.start("Application build");
-    return new Promise((resolve, reject) => {
-        webpack(config, (error, stats) => {
-            if (error) {
-                logger.error(error);
-                return reject(1);
+  timer.start("Application build");
+  return new Promise((resolve, reject) => {
+    webpack(config, (error, stats) => {
+      if (error) {
+        logger.error(error);
+        return reject(1);
+      }
+      if (!stats) {
+        return reject(1);
+      } else if (stats.hasErrors()) {
+        const jsonStats = stats.toJson();
+        if (jsonStats.errors && jsonStats.errors.length > 0) {
+          for (const e of jsonStats.errors as string[]) {
+            let prettyError = e.replace(conf.rootDir + "/", "");
+            if (isTemplateError(prettyError)) {
+              logger.warn(prettyError.replace("ng://", "").replace("$$_gendir", ""));
+            } else {
+              logger.error(prettyError);
             }
-            if (!stats) {
-                return reject(1);
-            } else if (stats.hasErrors()) {
-                const jsonStats = stats.toJson();
-                if (jsonStats.errors && jsonStats.errors.length > 0) {
-                    logger.error(jsonStats.errors[0]);
-                }
-                return reject(1);
-            }
-            if (argv["debug"]) {
-                fs.writeFileSync(path.join(conf.targetDir, "stats.json"), JSON.stringify(stats.toJson()), { encoding: "utf-8" });
-            }
-            timer.end("Application build");
-            resolve(0);
-        });
+          }
+        }
+        if (!onlyTemplateErrors(jsonStats.errors)) {
+          return reject(1);
+        }
+      }
+      if (argv["debug"]) {
+        fs.writeFileSync(path.join(conf.targetDir, "stats.json"), JSON.stringify(stats.toJson()), { encoding: "utf-8" });
+      }
+      timer.end("Application build");
+      resolve(0);
     });
+  });
 }
