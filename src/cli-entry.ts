e4e2c8b07dacf4a055df2df87ef068711b1c7e8c
Frederik Schubert <frederik.schubert@inside-m2m.de>
2018-06-04 16:19:29 +0200

    fix(*): do not use ts-node

--- src/cli-entry.ts
+++ src/cli-entry.ts
@@ -1,15 +1,8 @@
 #! /usr/bin/env node
 
-// tslint:disable-next-line:no-var-requires
-require("ts-node").register({
-    fast: true,
-    disableWarnings: true
-});
+import * as cli from "./cli";
 
 // Provide a title to the process in `ps`
 process.title = "slim";
-
-// tslint:disable-next-line:no-var-requires
-const cli = require("./cli");
 
 cli.main();
