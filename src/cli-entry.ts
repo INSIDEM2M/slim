291a11bffba756c9cb66a573c39b621dbee3db7e
Frederik Schubert <frederik.schubert@inside-m2m.de>
2018-06-05 12:39:30 +0200

    fix(cli-entry): do not typecheck slim when executing it

--- src/cli-entry.ts
+++ src/cli-entry.ts
@@ -1,8 +1,10 @@
 #! /usr/bin/env node
+const path = require("path");
 
 // tslint:disable-next-line:no-var-requires
 require("ts-node").register({
-    skipProject: true
+    project: path.resolve(__dirname, "..", "tsconfig.json"),
+    transpileOnly: true
 });
 
 // Provide a title to the process in `ps`
